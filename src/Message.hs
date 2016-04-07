e3f620c167e6a6eb97f4e3fe31a124bcaaa78ddb
James Larisch <jamesfordummies@gmail.com>
2016-04-07 19:50:41 -0400

    Add Message definition. Default aeson derivation causes large compile time...

--- src/Message.hs
+++ src/Message.hs
@@ -1,12 +1,39 @@
 module Message where
+import Data.Aeson
+import Data.Text
+import GHC.Generics
+import RoutingData
 
+-- let's assume network is reliable right? we don't need an ack...
+-- https://www.youtube.com/watch?v=JG2ESDGwHHY
 
-data Message = Message {
-  m :: String
-  }
+-- | These messages are sent between nodes.
+data Message
+  = Store
+  { src :: ID                -- ^ Who sent it?
+  , key :: Text              -- ^ Under what key are we storing the value?
+  , value :: Text            -- ^ What value are we storing?
+  , dest :: ID }             -- ^ Who is it destined for?
+  | FindNode
+  { src :: ID                -- ^ Who sent it?
+  , dest :: ID               -- ^ Who is it destined for?
+  , target :: ID }           -- ^ Who are we trying to find?
+  | FindNodeR
+  { src :: ID                -- ^ Who sent it?
+  , dest :: ID               -- ^ Who is it destined for?
+  , results :: [NodeInfo] }  -- ^ Who is close to the target?
+  | FindValue
+  { src :: ID                -- ^ Who sent it?
+  , dest :: ID               -- ^ Who is it destined for
+  , key :: Text }            -- ^ Under which key is the value we're trying to obtain?
+  | FindValueR
+  { src :: ID                -- ^ Who sent it?
+  , dest :: ID               -- ^ Who is it destined for?
+  , value :: Text }          -- ^ What value did we have under that key?
+  deriving (Show)
 
 deserialize :: String -> Message
-deserialize = Message
+deserialize s = undefined
 
 serialize :: Message -> String
-serialize = m
+serialize m = undefined
