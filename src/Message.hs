cfdd4385480ed1eae30914c7fa6bb73c84581fc2
James Larisch <jamesfordummies@gmail.com>
2016-04-12 19:21:29 -0400

    Add unique ID and time sent fields to messages
    We want to be able to identify a message, and we want to know when a
    message was sent so we can time it out.

--- src/Message.hs
+++ src/Message.hs
@@ -1,8 +1,11 @@
 module Message where
+import RoutingData
+
 import Data.Aeson
 import Data.Text
 import GHC.Generics
-import RoutingData
+import Data.Time.Clock
+
 
 -- let's assume network is reliable right? we don't need an ack...
 -- https://www.youtube.com/watch?v=JG2ESDGwHHY
@@ -10,26 +13,41 @@ import RoutingData
 -- | These messages are sent between nodes.
 data Message
   = Store
-  { src :: ID                -- ^ Who sent it?
-  , key :: ID                -- ^ Under what key are we storing the value?
-  , value :: Text            -- ^ What value are we storing?
-  , dest :: ID }             -- ^ Who is it destined for?
+  { src :: ID                          -- ^ Who sent it?
+  , key :: ID                          -- ^ Under what key are we storing the value?
+  , value :: Text                      -- ^ What value are we storing?
+  , dest :: ID                         -- ^ Who is it destined for?
+  , sent :: UTCTime                    -- ^ When was it sent?
+  , mID :: Text                        -- ^ Unique UUID for this message
+  }
   | FindNode
-  { src :: ID                -- ^ Who sent it?
-  , dest :: ID               -- ^ Who is it destined for?
-  , target :: ID }           -- ^ Who are we trying to find?
+  { src :: ID                          -- ^ Who sent it?
+  , dest :: ID                         -- ^ Who is it destined for?
+  , target :: ID                       -- ^ Who are we trying to find?
+  , sent :: UTCTime                    -- ^ When was it sent?
+  , mID :: Text                        -- ^ Unique UUID for this message
+  }
   | FindNodeR
-  { src :: ID                -- ^ Who sent it?
-  , dest :: ID               -- ^ Who is it destined for?
-  , results :: [NodeInfo] }  -- ^ Who is close to the target?
+  { src :: ID                          -- ^ Who sent it?
+  , dest :: ID                         -- ^ Who is it destined for?
+  , results :: [NodeInfo]              -- ^ Who is close to the target?
+  , sent :: UTCTime                    -- ^ When was it sent?
+  , mID :: Text                        -- ^ Unique UUID for this message
+  }
   | FindValue
-  { src :: ID                -- ^ Who sent it?
-  , dest :: ID               -- ^ Who is it destined for
-  , key :: ID }              -- ^ Under which key is the value we're trying to obtain?
+  { src :: ID                          -- ^ Who sent it?
+  , dest :: ID                         -- ^ Who is it destined for
+  , key :: ID                          -- ^ Under which key is the value we're trying to obtain?
+  , sent :: UTCTime                    -- ^ When was it sent?
+  , mID :: Text                        -- ^ Unique UUID for this message
+  }
   | FindValueR
-  { src :: ID                -- ^ Who sent it?
-  , dest :: ID               -- ^ Who is it destined for?
-  , value :: Text }          -- ^ What value did we have under that key?
+  { src :: ID                          -- ^ Who sent it?
+  , dest :: ID                         -- ^ Who is it destined for?
+  , value :: Text                      -- ^ What value did we have under that key?
+  , sent :: UTCTime                    -- ^ When was it sent?
+  , mID :: Text                        -- ^ Unique UUID for this message
+  }
   deriving (Show)
 
 deserialize :: String -> Message
