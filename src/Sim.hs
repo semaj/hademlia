b6fdec931970423bdea6a73c9f92eb741f10ce48
James Larisch <jamesfordummies@gmail.com>
2016-04-15 20:21:35 -0400

    Simulation fields

--- src/Sim.hs
+++ src/Sim.hs
@@ -5,6 +5,7 @@ import           RoutingData
 import           Utils
 
 import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
 import           Data.Time.Clock
 import           System.Random
 
@@ -43,12 +44,46 @@ randLog range@(lo, hi) gen logMe = do
   putStrLn $ "Generating " ++ logMe ++ "between " ++ (show lo) ++ " - " ++ (show hi)
   return $ randomR range gen
 
-initNodes :: RandomGen g => g -> UTCTime -> ((Node, Node), g)
-initNodes gen now = ((nodeA, nodeB), gen'')
+randomNode :: RandomGen g => g -> [Node] -> (Node, g)
+randomNode g nodes =
+  case maybeNode of
+    Nothing -> error $ "Node list no index " ++ (show i)
+    Just node -> (node, g')
+  where range = (0, length nodes - 1)
+        (i, g') = randomR range g
+        maybeNode = get nodes i
+
+initNodes :: RandomGen g => g -> UTCTime -> (HM.HashMap ID Node, g)
+initNodes gen now = (HM.fromList [(aID, nodeA), (bID, nodeB)], gen'')
    where (aID, gen') = randomID gen
          (bID, gen'') = randomID gen'
          nodeA = testNode aID bID now
          nodeB = testNode bID aID now
 
-start :: StdGen -> IO ()
-start gen = putStrLn "Yeah, I simulate things"
+execute :: Node -> Node
+execute node = node
+
+start :: RandomGen g => g -> IO ()
+start gen = do
+  now <- getCurrentTime
+  let (nodes, gen') = initNodes gen now
+  run gen' nodes [] [] HM.empty
+
+data Command = Put { key :: ID
+                   , value :: T.Text
+                   , triggered :: UTCTime
+                   , success :: Bool
+                   }
+             | Get { key :: ID
+                   , triggered :: UTCTime
+                   , success :: Bool
+                   }
+
+run :: RandomGen g => g         -- ^ The random number generator
+       -> HM.HashMap ID Node    -- ^ ID -> Node with that ID
+       -> [Message]             -- ^ Message queue between nodes
+       -> [Command]             -- ^ Commands sent
+       -> HM.HashMap ID T.Text  -- ^ Reference hashmap (DHT map should emulate this)
+       -> IO ()
+run g nodes messages commands refStore = do
+  putStrLn "sup"
